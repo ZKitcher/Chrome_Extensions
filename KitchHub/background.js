ba8e9a97e107d88a55c7d862199c53b66bd87073
Zac Kitcher <ZKitcher@HamiltonMFG.com>
2023-12-27 16:25:42 -0500

    🖼️ add(logo.png): add new logo image for KitchHub project 🔧 refactor(background.js): refactor createContextMenu function to remove existing context menus before creating new ones
    🐛 fix(content.js): fix missing newline at the end of the file
    ✨ feat(content.js): add support for running features based on hub configuration
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧 chore(content.js): refactor code to improve readability and maintainability
    🔧
    
    🔧 chore(manifest.json): add "storage" permission to manifest to enable storage API usage
    🔧 chore(popup.css): add CSS styles for the popup UI
    🔧 chore(popup.html): create HTML structure for the popup UI
    🔧 chore(popup.js): add JavaScript logic for toggling features and sending messages to content script

--- KitchHub/background.js
+++ KitchHub/background.js
@@ -1,36 +1,26 @@
 const LS_ID = 'LinkScan'
 const YT_ID = 'YouTubeSearch'
 
-const createdMenus = {}
 
-const createContextMenu = () => {
+function createContextMenu() {
+    chrome.contextMenus.removeAll(() => {
+        chrome.contextMenus.create({
+            id: YT_ID,
+            title: 'YouTube Search',
+            contexts: ['selection'],
+        });
 
-    chrome.contextMenus.create({
-        id: YT_ID,
-        title: 'YouTube Search',
-        contexts: ['selection'],
+        chrome.contextMenus.create({
+            id: LS_ID,
+            title: 'Link Search',
+            contexts: ['selection'],
+        });
     });
-
-    chrome.contextMenus.create({
-        id: LS_ID,
-        title: 'Link Search',
-        contexts: ['selection'],
-    });
-
     console.log('Context menu created.');
-    createdMenus[YT_ID] = true;
-    createdMenus[LS_ID] = true;
 }
 
-if (createdMenus[LS_ID]) {
-    console.log('Context menu already exists.');
-} else {
-    createContextMenu();
-}
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-
-});
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => { });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === LS_ID) {
@@ -44,3 +34,6 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
         chrome.tabs.create({ url: URL });
     }
 });
+
+
+createContextMenu();
--- KitchHub/content.js
+++ KitchHub/content.js
@@ -1,12 +1,31 @@
 const LS_ID = 'LinkScan'
 const YT_ID = 'YouTubeSearch'
+const storageKey = 'KitchHub'
+const href = window.location.href;
 
-if (window.location.href.match(/Twitch.tv/gi)) {
+const defaultConfig = {
+    twitch: true,
+    mediaDemo: true,
+    pageLinkLookup: true,
+    POE: true
+}
+
+let runningFeatures = defaultConfig;
+getHubConfig();
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+// Twitch.tv
+
+if (href.match(/Twitch.tv/gi)) {
+    AdMuter()
+}
+
+function AdMuter() {
     let volume = null;
     const MAIN_ID = 'MainStreamVideo'
     const AD_BANNER_ID = 'AdBanner'
-
-    const isVideoPlaying = (e) => !!(e.currentTime > 0 && !e.paused && !e.ended && e.readyState > 2);
 
     setInterval(() => {
         const videos = document.getElementsByTagName('video');
@@ -26,6 +45,8 @@ if (window.location.href.match(/Twitch.tv/gi)) {
                 }
             }
 
+            if (!runningFeatures.twitch) return;
+
             vidArray.forEach(e => {
                 if (!e.hasAttribute('controls')) {
                     e.setAttribute('controls', '');
@@ -44,17 +65,199 @@ if (window.location.href.match(/Twitch.tv/gi)) {
                 }
             });
         }
-    }, 500)
+    }, 200);
 
     // TODO add to title for ad 🔴
 }
 
-const stringSimilarityPercentage = (str1, str2) => {
-    function levenshteinDistance(s1, s2) {
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+// Media Demo
+
+let hoverTimer;
+
+document.addEventListener('mouseover', (e) => {
+    if (!runningFeatures.mediaDemo) return;
+
+    if (e.target.tagName === 'A') {
+        const HREF = e.target.getAttribute('href');
+        if (!HREF) return;
+
+        const demo = createDemo(HREF, e);
+        if (demo)
+            document.body.appendChild(demo);
+
+        e.target.addEventListener('mouseleave', function mouseLeaveHandler() {
+            const demo = document.querySelectorAll('#MousedOverDemo');
+            demo.forEach((e) => e.remove());
+            e.target.removeEventListener('mouseleave', mouseLeaveHandler);
+        }, { once: true });
+    }
+});
+
+
+function createDemo(href, event) {
+    let imageDemo = null;
+
+    if (href.match(/.jpg|.png|.gif/)) {
+        imageDemo = document.createElement('div');
+        imageDemo.style.backgroundImage = `url(${href})`;
+    }
+    if (href.match(/.gifv|.mp4/)) {
+        href = href.replace('.gifv', '.mp4')
+        imageDemo = document.createElement('video');
+        imageDemo.autoplay = true;
+        imageDemo.src = href;
+    }
+
+    if (!imageDemo) return;
+
+    const imageLimit = 500;
+
+    imageDemo.id = 'MousedOverDemo';
+    imageDemo.style.position = 'absolute';
+    imageDemo.style.top = `${event.pageY + 20}px`;
+    imageDemo.style.left = `${event.pageX}px`;
+    imageDemo.style.position = 'absolute';
+    imageDemo.style.backgroundRepeat = 'no-repeat';
+    imageDemo.style.backgroundPosition = 'center center';
+    imageDemo.style.backgroundSize = 'contain';
+    imageDemo.style.width = `${imageLimit}px`;
+    imageDemo.style.height = `${Math.min((window.outerHeight - 50) - event.screenY, imageLimit)}px`;
+    imageDemo.style.zIndex = '100';
+
+    return imageDemo;
+}
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+// Page Link Lookup
+
+function linkSearch(query) {
+    if (!runningFeatures.pageLinkLookup) return;
+
+    const allLinks = document.querySelectorAll('a');
+
+    const bestMatchLink = findBestMatchingLink(query, allLinks);
+
+    if (bestMatchLink) {
+        bestMatchLink.classList.add('LinkScan');
+        bestMatchLink.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+            inline: 'center'
+        });
+    } else {
+        console.log('No Matching Link found.');
+    }
+}
+
+function findBestMatchingLink(query, links) {
+    let bestMatch = 25;
+    let bestMatchLink = null;
+
+    links.forEach(linkElement => {
+        const linkText = linkElement.textContent;
+        const match = stringSimilarityPercentage(query, linkText);
+        if (match > bestMatch) {
+            bestMatch = match;
+            bestMatchLink = linkElement;
+        }
+    });
+
+    return bestMatchLink;
+};
+
+document.addEventListener('mousedown', () => {
+    const linkScan = document.querySelectorAll('.LinkScan')[0];
+    if (linkScan)
+        linkScan.classList.remove('LinkScan');
+
+    // const selectedText = window.getSelection().toString().trim();
+    // if (selectedText) {
+    //     chrome.runtime.sendMessage({ type: LS_ID, text: selectedText });
+    //     chrome.runtime.sendMessage({ type: YT_ID, text: selectedText });
+    // }
+});
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+// POE
+
+if (runningFeatures.POE) {
+    if (href.match(/pathofexile.fandom/gi)) {
+        window.location.href = 'https://www.poewiki.net' + window.location.pathname;
+    }
+    document.querySelectorAll('a[href*="pathofexile.fandom.com"]').forEach(e => {
+        e.href = e.href.replace('pathofexile.fandom.com', 'www.poewiki.net');
+    });
+};
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+// Cine2Cheatle
+
+var OriginalWebSocket = window.WebSocket;
+var callWebSocket = OriginalWebSocket.apply.bind(OriginalWebSocket);
+var wsAddListener = OriginalWebSocket.prototype.addEventListener;
+wsAddListener = wsAddListener.call.bind(wsAddListener);
+
+window.WebSocket = function WebSocket(url, protocols) {
+    var ws;
+    if (!(this instanceof WebSocket)) {
+        // Called without 'new' (browsers will throw an error).
+        ws = callWebSocket(this, arguments);
+    } else if (arguments.length === 1) {
+        ws = new OriginalWebSocket(url);
+    } else if (arguments.length >= 2) {
+        ws = new OriginalWebSocket(url, protocols);
+    } else { // No arguments (browsers will throw an error)
+        ws = new OriginalWebSocket();
+    }
+
+    wsAddListener(ws, 'open', function (event) {
+        console.log('WebSocket connection opened:', event);
+    });
+    wsAddListener(ws, 'close', function (event) {
+        console.log('WebSocket connection closed:', event);
+    });
+    wsAddListener(ws, 'message', function (event) {
+        console.log('WebSocket message received:', event.data);
+    });
+
+    return ws;
+}.bind();
+
+window.WebSocket.prototype = OriginalWebSocket.prototype;
+window.WebSocket.prototype.constructor = window.WebSocket;
+
+var wsSend = OriginalWebSocket.prototype.send;
+wsSend = wsSend.apply.bind(wsSend);
+
+window.WebSocket.prototype.send = function (data) {
+    console.log('WebSocket message sent:', data);
+    return wsSend(this, arguments);
+};
+
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+// Utility
+
+function isVideoPlaying(e) {
+    return !!(e.currentTime > 0 && !e.paused && !e.ended && e.readyState > 2);
+}
+
+function stringSimilarityPercentage(str1, str2) {
+    const levenshteinDistance = (s1, s2) => {
         const m = s1.length;
         const n = s2.length;
         const dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
-
         for (let i = 0; i <= m; i++) {
             for (let j = 0; j <= n; j++) {
                 if (i === 0) {
@@ -71,7 +274,6 @@ const stringSimilarityPercentage = (str1, str2) => {
                 }
             }
         }
-
         return dp[m][n];
     }
 
@@ -82,117 +284,53 @@ const stringSimilarityPercentage = (str1, str2) => {
     return parseFloat(similarityPercentage.toFixed(2));
 }
 
-const findBestMatchingLink = (query, links) => {
-    let bestMatch = 25;
-    let bestMatchLink = null;
 
-    links.forEach(linkElement => {
-        const linkText = linkElement.textContent;
-        const match = stringSimilarityPercentage(query, linkText);
-        if (match > bestMatch) {
-            bestMatch = match;
-            bestMatchLink = linkElement;
-        }
-    });
-
-    return bestMatchLink;
-}
-
-const createDemo = (href, event) => {
-    let imageDemo = null;
-
-    if (href.match(/.jpg|.png|.gif/)) {
-        imageDemo = document.createElement('div');
-        imageDemo.style.backgroundImage = `url(${href})`;
-    }
-    if (href.match(/.gifv|.mp4/)) {
-        href = href.replace('.gifv', '.mp4')
-        imageDemo = document.createElement('video');
-        imageDemo.autoplay = true;
-        imageDemo.src = href;
-    }
-
-    if (!imageDemo) {
-        return;
-    }
-
-    imageDemo.id = 'MousedOverDemo';
-    imageDemo.style.position = 'absolute';
-    imageDemo.style.top = `${event.pageY + 20}px`;
-    imageDemo.style.left = `${event.pageX}px`;
-    imageDemo.style.position = 'absolute';
-    imageDemo.style.backgroundRepeat = 'no-repeat';
-    imageDemo.style.backgroundPosition = 'center center';
-    imageDemo.style.backgroundSize = 'contain';
-    imageDemo.style.width = '400px';
-    imageDemo.style.height = '400px';
-    imageDemo.style.zIndex = '100';
-
-    return imageDemo;
-}
-
-document.addEventListener('mouseover', (e) => {
-    if (e.target.tagName === "A") {
-
-        const HREF = e.target.getAttribute('href');
-
-        if (!HREF) return;
-
-        const demo = createDemo(HREF, e);
-
-        if (demo)
-            document.body.appendChild(demo);
-
-        e.target.addEventListener("mouseleave", function mouseLeaveHandler() {
-            const demo = document.querySelectorAll('#MousedOverDemo');
-            demo.forEach((e) => e.remove());
-            e.target.removeEventListener("mouseleave", mouseLeaveHandler);
-        }, { once: true });
-    }
-
-
-});
-
-document.addEventListener('mousedown', () => {
-    const linkScan = document.querySelectorAll('.LinkScan')[0];
-    if (linkScan)
-        linkScan.classList.remove('LinkScan');
-
-    const selectedText = window.getSelection().toString().trim();
-    if (selectedText) {
-        // chrome.runtime.sendMessage({ type: LS_ID, text: selectedText });
-        // chrome.runtime.sendMessage({ type: YT_ID, text: selectedText });
-    }
-});
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+// Listeners
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     try {
         if (message.id === LS_ID) {
-            const query = message.message;
-            const allLinks = document.querySelectorAll('a');
-
-            const bestMatchLink = findBestMatchingLink(query, allLinks);
-
-            if (bestMatchLink) {
-                bestMatchLink.classList.add('LinkScan');
-                bestMatchLink.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'center',
-                    inline: 'center'
-                });
-            } else {
-                console.log('No Matching Link found.');
-            }
+            linkSearch(message.message);
         }
+
+        if (message.command) {
+            if (message.command === 'enable-feature') {
+                // Enable some feature
+                runningFeatures[message.feature] = true;
+
+            } else if (message.command === 'disable-feature') {
+                // Disable some feature
+                runningFeatures[message.feature] = false;
+            }
+            setHubConfig();
+        }
+
     } catch (error) {
         console.error('Error:', error);
     }
 });
 
 
-document.querySelectorAll('a[href*="pathofexile.fandom.com"]').forEach(e => {
-    e.href = e.href.replace('pathofexile.fandom.com', 'www.poewiki.net');
-});
-if (window.location.href.match(/pathofexile.fandom/gi)) {
-    window.location.href = 'https://www.poewiki.net' + window.location.pathname;
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+// Hub Config
+
+function getHubConfig() {
+    try {
+        chrome.storage.sync.get(null, function (result) {
+            runningFeatures = { ...runningFeatures, ...result };
+        });
+    } catch {
+        resolve(defaultConfig)
+    }
+}
+
+function setHubConfig() {
+    chrome.storage.sync.set(runningFeatures, function () {
+        console.log('Values are set to sync storage', runningFeatures);
+    });
 }
